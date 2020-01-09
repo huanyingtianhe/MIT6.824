1c6ff61bbc09e368140c8b8798021678e3a1c916
Connor Chen <chenxiaokang97@163.com>
2020-01-09 22:21:06 +0800

    add: add rpc

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -19,3 +19,21 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+// worker to master
+type register struct {
+	host string
+}
+
+type registerReply struct {
+	ok bool
+}
+
+// master to worker
+type assignTask struct {
+	task string
+	path string
+}
+
+type assignTaskReply struct {
+	ok bool
+}
